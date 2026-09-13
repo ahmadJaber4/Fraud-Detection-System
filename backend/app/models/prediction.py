e4fda3f37dc16c59bfef875ddfd4fdce09d2cb60
Ahmad Jaber <ahmadjaberfort@gmail.com>
2026-09-13 15:00:41 +0300

    define ORM model for predections table

---
+++ backend/app/models/prediction.py
@@ -0,0 +1,23 @@
+# import libraries
+from sqlalchemy import Column, Integer, Float, String, DateTime
+from sqlalchemy.sql import func
+from app.database.database import Base
+
+class Prediction(Base):
+    __tablename__ = 'predictions'
+
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now, nullable=False)
+
+    # input features
+    amount = Column(Float, nullable=False)
+    location = Column(String, nullable=False)
+    device_type = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    income = Column(Float, nullable=False)
+    debt = Column(Float, nullable=False)
+    credit_score = Column(Integer, nullable=False)
+
+    # model output
+    predicted_label = Column(Integer, nullable=False) # 0 or 1
+    predicted_probability = Column(Float, nullable=False)
