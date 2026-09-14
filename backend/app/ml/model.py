d2b59419113c4f4c02b45d11a77d7da69a10aab5
Ahmad Jaber <ahmadjaberfort@gmail.com>
2026-09-14 15:34:37 +0300

    load model and define predict function

---
+++ backend/app/ml/model.py
@@ -0,0 +1,18 @@
+# import libraries
+from pathlib import Path
+import joblib
+import pandas as pd
+
+# locate model file
+MODEL_PATH = Path(__file__).resolve().parent / "fraud_model.pkl"
+
+# load pipeline (preprocessing + XGBoost)
+model = joblib.load(MODEL_PATH)
+
+# predict function
+def predict(features: dict) -> tuple[int, float]:
+    input_df = pd.DataFrame([features])
+    predicted_label = int(model.predict(input_df)[0])
+    predicted_probability = float(model.predict_proba(input_df)[0][1])
+    
+    return predicted_label, predicted_probability
