0eb35737f6246de23f3a369e719bcae741997943
Ahmad Jaber <ahmadjaberfort@gmail.com>
2026-09-17 16:47:45 +0300

    create frontend files and routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom"
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<PredictForm/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
+  )
+}
+
+export default App
