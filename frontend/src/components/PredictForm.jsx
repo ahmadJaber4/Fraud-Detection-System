4469277c551c71a983b5d9b2f31c034b2f74af24
Ahmad Jaber <ahmadjaberfort@gmail.com>
2026-09-19 15:21:03 +0300

    add form UI

--- frontend/src/components/PredictForm.jsx
+++ frontend/src/components/PredictForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Header from "./Header";
 
-export default function PredictForm(){
+export default function PredictForm() {
     const [inputData, setInputData] = useState({
         amount: null,
         location: null,
@@ -14,7 +14,55 @@ export default function PredictForm(){
 
     return (
         <>
-            <Header/>
+            <Header />
+            <h3 className="form-title">Enter transaction details:</h3>
+            <div className="predict-form">
+                <div className="input">
+                    <label>Amount</label>
+                    <input type="text" className="text-input" />
+                </div>
+
+                <div className="input">
+                    <label>Location</label>
+                    <input type="text" className="text-input" />
+                </div>
+
+                <div className="input">
+                    <label>Device Type</label>
+                    <select name="device" id="device">
+                        <option value="mobile">Mobile</option>
+                        <option value="desktop">Desktop</option>
+                        <option value="tablet">Tablet</option>
+                    </select>
+                </div>
+
+                <div className="input">
+                    <label>Age</label>
+                    <input type="number" min='18' max='100' />
+                </div>
+
+                <div className="input">
+                    <label>Income</label>
+                    <input type="text" className="text-input" />
+                </div>
+
+                <div className="input">
+                    <label>Debt</label>
+                    <input type="text" className="text-input" />
+                </div>
+
+                <div className="input">
+                    <label>Credit Score</label>
+                    <input type="number" min='300' max='850' />
+                </div>
+                
+                <div className="buttons-box">
+                    <button className="btn analyze-btn">Analyze</button>
+                    <button className="btn clear-btn">Clear</button>
+                </div>
+
+                <div className="result-box"></div>
+            </div>
         </>
     )
 }
